98a38ba961f6cb80ca79fd49d051ba803072661b
imrancoder10 <imranhossain1148@gmail.com>
2024-08-31 01:55:57 +0600

    task4 isNaN

---
+++ task4.js
@@ -0,0 +1,18 @@
+/**
+ * ### Task-4
+
+What will be the result of the following codes:
+
+**var a = isNaN(‘11’);**
+
+**var a = isNaN(2-10);**
+
+Explain your answers.
+
+ */
+
+var a=isNaN('Imran');
+console.log(a);
+
+var b=isNaN(2-8);
+console.log(b);
